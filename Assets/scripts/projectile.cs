998a887938b349bed0f64d0dfe5eef1a92f1c578
BaoLanNguyen <bang03001@utb.vaxjo.se>
2022-11-08 12:43:11 +0100

    Summary

--- Assets/scripts/projectile.cs
+++ Assets/scripts/projectile.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class projectile : MonoBehaviour
 {
-    public Transform FirePoint;
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
-    {   
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-        }
+        
     }
 }
