f159fc0ef85bee2d8d6335f014369504305c7b6e
BaoLanNguyen <bang03001@utb.vaxjo.se>
2022-11-17 12:29:04 +0100

    summary

--- Assets/scripts/projectile.cs
+++ Assets/scripts/projectile.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class projectile : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private bool hit;
+    private BoxCollider2D boxCollider;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hit) return;
+        float movementSpeed = speed * Time.deltaTime;
     }
 }
