132c6e59e330eb8dbf6f6dfe841033f1238c92b8
BaoLanNguyen <bang03001@utb.vaxjo.se>
2022-11-22 13:19:17 +0100

    Revert "Merge branch 'master' of https://github.com/BaoLanNguyen/GYA"
    This reverts commit 78e1b51bb68ce8a0178bffd82afbff952fbdfc37, reversing
    changes made to 3abe7bd759b5d32a45042258fddd211f1bb12f42.

--- Assets/scripts/GameOverManager.cs
+++ Assets/scripts/GameOverManager.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GameOverManager : MonoBehaviour
 {   public GameObject gameOverUI;
-    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
     public void gameOver(){
         gameOverUI.SetActive(true);
     }
