51d67b8322dead98a6f5eefa6a2779476fe7a7f7
HibbaHindawi <nhlpool27@gmail.com>
2022-12-07 15:27:01 +0100

    up

---
+++ Assets/scripts/EnemyPatrolR.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolR : MonoBehaviour
+{
+     [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+    [SerializeField] private Transform enemy;
+    [SerializeField]private float speed;
+    private Vector3 InScale;
+    private bool MovingLeft;
+    private void Awake() {
+        InScale = enemy.localScale;
+    }
+    private void Update() {
+        if (MovingLeft)
+        {    if(enemy.position.x >= leftEdge.position.x) 
+             MoveInDirection(-1);
+             else
+             DirectionChange();
+        }
+        else
+        {
+            if(enemy.position.x <= rightEdge.position.x)
+            MoveInDirection(1);
+            else
+            DirectionChange();
+        }
+    }
+   private void DirectionChange()
+   {
+    MovingLeft= !MovingLeft;
+   }
+   private void MoveInDirection( int _direction)
+    {        enemy.localScale = new Vector3(Mathf.Abs(InScale.x) * _direction,InScale.y,InScale.z);
+             enemy.position = new Vector3(enemy.position.x + Time.deltaTime* _direction* speed, enemy.position.y, enemy.position.z);
+    }
+}
