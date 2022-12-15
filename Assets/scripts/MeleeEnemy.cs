8f7c3edd532be49deee0d10ca12a16e5eb8f1c52
BaoLanNguyen <bang03001@utb.vaxjo.se>
2022-12-15 12:26:59 +0100

    su

--- Assets/scripts/MeleeEnemy.cs
+++ Assets/scripts/MeleeEnemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MeleeEnemy : MonoBehaviour
 {
     [SerializeField] private float attackCD;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;
     [SerializeField] private BoxCollider2D boxCollider;
     private float CDTimer = Mathf.Infinity;
@@ -15,14 +17,18 @@ public class MeleeEnemy : MonoBehaviour
         if (PlayerInSight()){
             if (CDTimer >= attackCD)
         {
-            
+            CDTimer = 0;
         }
     }
     }
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.left, 0, playerLayer);
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right*range*transform.localScale.x*colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
         return hit.collider != null;
+    }
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right*range*transform.localScale.x*colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 }
 
