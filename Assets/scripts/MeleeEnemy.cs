01bf12f562ed491bd8f4701f1f3bad7a1f01752b
BaoLanNguyen <bang03001@utb.vaxjo.se>
2022-12-16 13:50:59 +0100

    su

--- Assets/scripts/MeleeEnemy.cs
+++ Assets/scripts/MeleeEnemy.cs
@@ -12,6 +12,7 @@ public class MeleeEnemy : MonoBehaviour
     private float CDTimer = Mathf.Infinity;
     [SerializeField] private LayerMask playerLayer;
     private Animator anim;
+    private Health playerHealth;
     private void Awake() {
         anim = GetComponent<Animator>();
     }
@@ -28,7 +29,11 @@ public class MeleeEnemy : MonoBehaviour
     private bool PlayerInSight()
     {
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right*range*transform.localScale.x*colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
-        return hit.collider != null;
+        if (hit.collider != null)
+        {
+            playerHealth = hit.transform.GetComponent<Health>();
+        }    
+       return hit.collider != null;
     }
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
@@ -36,7 +41,7 @@ public class MeleeEnemy : MonoBehaviour
     }
     private void DamagePlayer(){
         if (PlayerInSight()){
-            
+            playerHealth.takeDamage(damage);
         }
     }
 }
