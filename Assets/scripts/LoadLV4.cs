88b933e3257fc29adf236208089beeb37d587039
Pingvu <nhlpool27@gmail.com>
2023-03-16 12:22:25 +0100

    ändringar

---
+++ Assets/scripts/LoadLV4.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadLV4 : MonoBehaviour
+{
+    public void LoadLevel4(){
+    SceneManager.LoadScene("Level4");
+}
+}
+
