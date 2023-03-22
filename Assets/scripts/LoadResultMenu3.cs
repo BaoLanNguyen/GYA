fb2dfdd98e3f3acc4f57811510cd96cfb639d97c
Pingvu <nhlpool27@gmail.com>
2023-03-22 14:56:02 +0100

    up

---
+++ Assets/scripts/LoadResultMenu3.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadResultMenu3 : MonoBehaviour
+{
+      private void OnTriggerEnter2D(Collider2D flag) {
+    SceneManager.LoadScene("ResultMenu3");
+      }
+}
